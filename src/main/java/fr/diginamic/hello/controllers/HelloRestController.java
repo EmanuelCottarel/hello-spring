eb361d0203b3dc0cd70ecacab260501be42bba96
EmanuelCottarel <emanuelcottarel@gmail.com>
2024-10-08 09:24:26 +0200

    init

--- src/main/java/fr/diginamic/hello/controllers/HelloRestController.java
+++ src/main/java/fr/diginamic/hello/controllers/HelloRestController.java
@@ -1,4 +1,4 @@
-package fr.diginamic.hello.controleurs;
+package fr.diginamic.hello.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
-public class HelloControleur {
+public class HelloRestController {
 
     @GetMapping
     public String direHello(){
